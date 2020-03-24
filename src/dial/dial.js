04c02355c33ab59604b9866604ff485774bd58ef
root <zuoxiu.xing@gmail.com>
2020-03-23 20:19:22 -0600

    Added backspace function

--- src/dial/dial.js
+++ src/dial/dial.js
@@ -1,3 +1,8 @@
 function press_num_key(num){
     document.getElementById("phoneNum").value += num;
 }
+
+function backspace(){
+    text = document.getElementById("phoneNum").value
+    document.getElementById("phoneNum").value = text.slice(0,-1);
+}
